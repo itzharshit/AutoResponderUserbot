9e1ed3249c794a017dbed7a7fe50a57a9810db06
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-20 00:40:37 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,46 +1,29 @@
-from telethon import TelegramClient, events 
+
 from telethon.sessions import StringSession
-# Replace the values below with your own API ID, API Hash and session file name
+from telethon import TelegramClient, events, sync
+
+# replace these values with your own api_id, api_hash, and session name
 api_id = 6
-SESSION = '1ApWapzMBuxnxUHxe-hALnT9r0mCe_uUR8-fdMVuSWfO8xyVL18PUHjdr37Cbq8XJUGkXJeqBqypfpxp5JApT4xWg1Ac6-UznAx_EMnhZJ4JFmaYl1PoRbD8_oClvj9lkD-ItkVW23Pfi_VFswCEha8KOQgts3wCduGZJzuokdb4-263G_fchji72I6T3g4nDC8JZ-wiXJXR__9BW0MTmIs8dZ6ixkygVu9pGHh2Iwdb-3auDGmqxe8T-FMwU3Q7tgVQJyD4rbucfhjDDhAOL2B9PHINucfVrIo0kvj0HhWdxljnYS6_T8Yk5viYdAH-fY0heIe2bp6YlGh6HX4sTX2zd_9v1cbM='
 api_hash = 'eb06d4abfb49dc3eeb1aeb98ae0f581e'
-session_name = 'my_session'
-# Create a new Telegram client
+session_name = 'YOUR_SESSION_NAME'
+SESSION = '1ApWapzMBuxnxUHxe-hALnT9r0mCe_uUR8-fdMVuSWfO8xyVL18PUHjdr37Cbq8XJUGkXJeqBqypfpxp5JApT4xWg1Ac6-UznAx_EMnhZJ4JFmaYl1PoRbD8_oClvj9lkD-ItkVW23Pfi_VFswCEha8KOQgts3wCduGZJzuokdb4-263G_fchji72I6T3g4nDC8JZ-wiXJXR__9BW0MTmIs8dZ6ixkygVu9pGHh2Iwdb-3auDGmqxe8T-FMwU3Q7tgVQJyD4rbucfhjDDhAOL2B9PHINucfVrIo0kvj0HhWdxljnYS6_T8Yk5viYdAH-fY0heIe2bp6YlGh6HX4sTX2zd_9v1cbM='
+# replace this message with the message you want to send to users when you are offline
+offline_message = 'Sorry, I am currently offline. I will respond to your message as soon as possible.'
+
 client = TelegramClient(StringSession(SESSION) , api_id, api_hash)
-# Define the message to be sent as an auto-reply
-auto_reply_message = 'Thanks for your message! I will get back to you as soon as possible.'
-# Define the event handler for incoming messages
+
+# register an event handler for incoming messages
 @client.on(events.NewMessage)
-async def handle_incoming_message(event):
+async def handle_new_message(event):
+    # check if the message was sent by a user (as opposed to a bot or a group)
+    if event.is_private:
+        # check if you are currently online
+        if client.is_user_authorized():
+            # if you are online, do nothing (let the message go through to your account)
+            return
+        else:
+            # if you are offline, send the offline message to the user who sent the message
+            await event.respond(offline_message)
 
-    if not await client.is_user_authorized() or event.out:
-
-        # User is not currently authorized or message is outgoing, ignore
-
-        return
-
-    
-
-    # Check if user is online
-
-    me = await client.get_me()
-
-    if me.status == 'online':
-
-        # User is currently online, ignore the message
-
-        return
-
-    
-
-    # Send the auto-reply message to the sender of the incoming message
-
-    await event.send_message(auto_reply_message)
-
-# Start the client
-
+# start the client
 client.start()
-
-# Run the client until it is disconnected
-
-client.run_until_disconnected()

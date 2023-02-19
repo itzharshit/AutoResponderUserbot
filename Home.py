e3773c910c1ca635f03814e0988a763b52a9f97a
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-19 22:34:07 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,11 +2,11 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 # Replace the values below with your own API ID, API Hash and session file name
 api_id = 6
-string= '1ApWapzMBuxnxUHxe-hALnT9r0mCe_uUR8-fdMVuSWfO8xyVL18PUHjdr37Cbq8XJUGkXJeqBqypfpxp5JApT4xWg1Ac6-UznAx_EMnhZJ4JFmaYl1PoRbD8_oClvj9lkD-ItkVW23Pfi_VFswCEha8KOQgts3wCduGZJzuokdb4-263G_fchji72I6T3g4nDC8JZ-wiXJXR__9BW0MTmIs8dZ6ixkygVu9pGHh2Iwdb-3auDGmqxe8T-FMwU3Q7tgVQJyD4rbucfhjDDhAOL2B9PHINucfVrIo0kvj0HhWdxljnYS6_T8Yk5viYdAH-fY0heIe2bp6YlGh6HX4sTX2zd_9v1cbM='
+SESSION = '1ApWapzMBuxnxUHxe-hALnT9r0mCe_uUR8-fdMVuSWfO8xyVL18PUHjdr37Cbq8XJUGkXJeqBqypfpxp5JApT4xWg1Ac6-UznAx_EMnhZJ4JFmaYl1PoRbD8_oClvj9lkD-ItkVW23Pfi_VFswCEha8KOQgts3wCduGZJzuokdb4-263G_fchji72I6T3g4nDC8JZ-wiXJXR__9BW0MTmIs8dZ6ixkygVu9pGHh2Iwdb-3auDGmqxe8T-FMwU3Q7tgVQJyD4rbucfhjDDhAOL2B9PHINucfVrIo0kvj0HhWdxljnYS6_T8Yk5viYdAH-fY0heIe2bp6YlGh6HX4sTX2zd_9v1cbM='
 api_hash = 'eb06d4abfb49dc3eeb1aeb98ae0f581e'
 session_name = 'my_session'
 # Create a new Telegram client
-client = TelegramClient(api_id, api_hash, string)
+client = TelegramClient(StringSession(SESSION) , api_id, api_hash)
 # Define the message to be sent as an auto-reply
 auto_reply_message = 'Thanks for your message! I will get back to you as soon as possible.'
 # Define the event handler for incoming messages
